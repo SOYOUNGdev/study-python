1138e2e5f2988b4aaa28fe2eddfbde71f903fcc2
SOYOUNGdev <soyoungim.sy@gmail.com>
2024-01-15 00:20:52 +0900

    은행 출금 내역 수정 완료

--- q_file/task_test/log.py
+++ q_file/task_test/log.py
@@ -7,9 +7,13 @@ def log_time(original_function):
 
         with open('log.txt', 'a', encoding='utf-8') as file:
                 if not error_code:
+                    # print(original_function.__name__)
+                    # print(original_function)
                     now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-                    if original_function == self.withdraw:
+                    # 원래 함수가 출금이면, 잔액 - 출금액
+                    if original_function.__name__ == 'withdraw':
                         file.write(f'예금주: {self.user_name}\t계좌번호: {self.user_account}\t잔액: {self.user_balance - money}\t거래시간: {now}\n')
+                    # 원래 함수가 입금이면, 잔액 + 입금액
                     else:
                         file.write(f'예금주: {self.user_name}\t계좌번호: {self.user_account}\t잔액: {self.user_balance + money}\t거래시간: {now}\n')
                 else:
